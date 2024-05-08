302d9da97345751be727c02ca60865a35dc35f19
Pedro <pedroselecto@gmail.com>
2024-05-08 14:42:41 -0300

    aula 13.2

---
+++ aula 13/gerente.py
@@ -0,0 +1,12 @@
+from funcionario import Funcionario
+
+class Gerente(Funcionario):
+    def __init__(self, nome, email, matricula):
+        super().__init__(nome, email, matricula)
+
+    def autorizar_abertura_conta(self, numero):
+        return numero <= 10
+    
+
+
+
